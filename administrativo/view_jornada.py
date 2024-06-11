47190e4bb2c619f5f1a468ed292f329fd5912b0c
kevinruiz <proyectospersonales1995@outlook.com>
2024-06-11 16:52:02 -0500

    Módulo jornadas

--- administrativo/view_jornada.py
+++ administrativo/view_jornada.py
@@ -176,8 +176,8 @@ def view(request):
                 page_number = request.GET.get('page')
                 page_obj = paginator.get_page(page_number)
                 data['page_obj'] = page_obj
-
-                return render(request, "jornadas/inicio.html", data)
+                data['DIAS_SEMANA'] = DIAS_SEMANA
+                return render(request, "jornadas/view.html", data)
             except Exception as ex:
                 print('Error on line {}'.format(ex.exc_info()[-1].tb_lineno))
 
