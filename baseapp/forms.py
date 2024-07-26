201ae8a36c3f9124598e8d0014f94dff4637f4d5
kevinruiz <proyectospersonales1995@outlook.com>
2024-07-26 15:07:47 -0500

    Placeholder

--- baseapp/forms.py
+++ baseapp/forms.py
@@ -27,8 +27,8 @@ class PersonaForm(forms.ModelForm):
         self.fields['ruc'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-4'})
         self.fields['direccion'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-12', 'required':'true'})
         self.fields['genero'].widget.attrs.update({'class': 'form-control', 'data-live-search':'true', 'col': 'col-md-6', 'required':'true'})
-        self.fields['fecha_nacimiento'].widget.attrs.update({'class': 'form-control date', 'col': 'col-md-6', 'type': 'date', 'format': 'dd-mm-yyyy', 'required':'true'})
-        self.fields['correo_electronico'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-6'})
+        self.fields['fecha_nacimiento'].widget.attrs.update({'class': 'form-control date', 'col': 'col-md-6', 'type': 'date', 'format': 'dd-mm-yyyy', 'required':'true', 'placeholder': '01/01/2024'})
+        self.fields['correo_electronico'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-6', 'placeholder': 'micorreo@outlook.com'})
         self.fields['telefono'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-6'})
         self.fields['foto'].widget.attrs.update({'class': 'form-control', 'col': 'col-md-12'})
 
